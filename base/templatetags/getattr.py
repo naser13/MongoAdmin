17ef8487bb307893c38fc1869cd0e0a416520f7c
Unknown <naser.aliabadi@gmail.com>
2017-12-03 18:10:40 +0330

    better fix :)

--- base/templatetags/getattr.py
+++ base/templatetags/getattr.py
@@ -8,11 +8,9 @@ def getattribute(value, arg):
     if "." in str(arg):
         firstarg = str(arg).split(".")[0]
         value = getattribute(value, firstarg)
-        if value is None:
-            return '-'
         arg = ".".join(str(arg).split(".")[1:])
         return getattribute(value, arg)
     try:
         return value[arg]
-    except KeyError:
+    except (KeyError, TypeError):
         return '-'
