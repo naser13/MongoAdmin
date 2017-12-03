5530ee9e71bcac8d36bfc23ec59a2c13c8167160
Unknown <naser.aliabadi@gmail.com>
2017-12-03 18:09:31 +0330

    fix getattr

--- base/templatetags/getattr.py
+++ base/templatetags/getattr.py
@@ -8,7 +8,7 @@ def getattribute(value, arg):
     if "." in str(arg):
         firstarg = str(arg).split(".")[0]
         value = getattribute(value, firstarg)
-        if value == '-':
+        if value is None:
             return '-'
         arg = ".".join(str(arg).split(".")[1:])
         return getattribute(value, arg)
