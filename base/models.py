7e8374272124a69fc38efd0cada5cc1ea9844a97
Naser Aliabadi <naser.aliabadi@gmail.com>
2018-01-09 15:54:44 +0330

    cache size

--- base/models.py
+++ base/models.py
@@ -9,7 +9,7 @@ TYPES_NOT_SHOWN = ['Object', 'ObjectId']
 variety_command = """%s %s --quiet --eval "var collection = '%s', outputFormat='json'" variety.js"""
 
 
-@lru_cache
+@lru_cache(maxsize=128)
 def get_collection_keys(db_name, collection_name):
     p = subprocess.Popen(variety_command % (settings.MONGO_PATH, db_name, collection_name), shell=True,
                          stdout=subprocess.PIPE)
