46109354d6d831f0a4de2e68f190400d94a2a7ce
Naser Aliabadi <naser.aliabadi@gmail.com>
2018-03-25 16:27:52 +0430

    better days

--- base/management/commands/device_days.py
+++ base/management/commands/device_days.py
@@ -11,17 +11,19 @@ class Command(BaseCommand):
     help = 'get devices use days'
 
     def handle(self, *args, **options):
-        uninstalls = defaultdict(lambda: 0)
+        uninstalls = defaultdict(lambda: set())
 
         devices = client['joojoo']['deviceDB'].find({"firstDate": {"$exists": True, "$ne": None}})
         for device in devices:
             first_date = datetime.datetime.fromtimestamp(int(device["firstDate"] / 1000))
+            if first_date.date() >= (datetime.datetime.now() - datetime.timedelta(days=1)).date():
+                continue
             last_date = datetime.datetime.fromtimestamp(int(device["lastDate"] / 1000))
             if first_date == last_date:
-                uninstalls[-1] += 1
+                uninstalls[-1].add(device["info"]["pusheId"])
             else:
                 usage_duration = last_date - first_date
-                uninstalls[usage_duration.days] += 1
+                uninstalls[usage_duration.days].add(device["info"]["pusheId"])
 
-        for uninstall in uninstalls.items():
-            print(uninstall)
+        for days, ids in uninstalls.items():
+            print(days, len(list(ids)))
